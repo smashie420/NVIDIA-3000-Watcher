5c17b09d047ba18304f261e092a8ee092e9a6b47
smashie420 <devinangespam@gmail.com>
2020-12-19 18:08:51 -0800

    📝Reinvented the wheel (myLoop)
    •This update will hopefully fix some issues on linux & windows
    • Add discord webhooks!
    
    [ADDED]
    • cooldown()
    • Discord Webhooks
    •Better errors
    •Check for invalid sites
    •Actual working cooldowns & multiple sites (best buy links) checks
    
    [CHANGED]
    •webScrape()
    •myLoop()

--- database.js
+++ database.js
@@ -45,6 +45,7 @@ async function sendMail(data){
     }
 }
 let urls = [];
+let discrodWebHook;
 
 function starter(){
   rl.question(consoleTitle + "Enter Url: ", function saveInput(url){
@@ -60,59 +61,75 @@ function enterMoreUrls(){
         enterMoreUrls() 
       })
     }else{
+      askWebhook()
+    }
+  })
+}
+function askWebhook(){
+  rl.question(`${consoleTitle}Do you want to use discord webhooks? (y/n): `, function saveInput(webhooksOption){
+    if(stringToBoolean(webhooksOption)){
+      rl.question(`${consoleTitle} Enter Webhook URL: `, function saveInput(webhookURL){
+        discrodWebHook = webhookURL
+        runQuestions()
+      })
+    }else{
+      discrodWebHook = ""
       runQuestions()
     }
   })
 }
 
 function runQuestions(){
-    rl.question(consoleTitle + "Enter Phone Number: ", function saveInput(phoneNum){
-      console.log(consoleTitle + "List of Carriers: AT&T, Verizon, Sprint, TMobile, Virgin Mobile, Nextel, Boost, Alltel, EE")
-      rl.question(consoleTitle + "Enter Carrier: ", function saveInput(carrier){
-        console.log(consoleTitle + "We need your gmail credentials to send SMS\n" + consoleTitle + "Gmail Username should be something like johndoe@gmail.com")
-        rl.question(consoleTitle + "Gmail username: ", function saveInput(gmailUser){
-          rl.question(consoleTitle + "Gmail Password: ", function saveInput(gmailPass){
-            rl.question(consoleTitle + "Would you like to test SMS? (y/n): ", async function saveInput(testEmail){
-              var sms = () => {
-                switch(carrier.toLocaleLowerCase().trim()){
-                  case "at&t": return phoneNum+"@txt.att.net"
-                  case "verizon": return phoneNum+"@vtext.com"
-                  case "sprint": return phoneNum+"@messaging.sprintpcs.com"
-                  case "tmobile": return phoneNum+"@tmomail.net"
-                  case "virgin mobile": return phoneNum+"@vmobl.com"
-                  case "nextel": return phoneNum+"@messaging.nextel.com"
-                  case "boost": return phoneNum+"@myboostmobile.com"
-                  case "alltel": return phoneNum+"@message.alltel.com"
-                  case "ee": return phoneNum+"@mms.ee.co.uk"
-                  case "default": return "Invalid Carrier Input"
-                }
-              } 
-              
-              let data = {
-                url : urls,
-                phoneNum : phoneNum,
-                carrier : carrier,
-                sms : sms(),
-                gmailUser: gmailUser,
-                gmailPass: gmailPass,
+    
+  
+  rl.question(consoleTitle + "Enter Phone Number: ", function saveInput(phoneNum){
+    console.log(consoleTitle + "List of Carriers: AT&T, Verizon, Sprint, TMobile, Virgin Mobile, Nextel, Boost, Alltel, EE")
+    rl.question(consoleTitle + "Enter Carrier: ", function saveInput(carrier){
+      console.log(consoleTitle + "We need your gmail credentials to send SMS\n" + consoleTitle + "Gmail Username should be something like johndoe@gmail.com")
+      rl.question(consoleTitle + "Gmail username: ", function saveInput(gmailUser){
+        rl.question(consoleTitle + "Gmail Password: ", function saveInput(gmailPass){
+          rl.question(consoleTitle + "Would you like to test SMS? (y/n): ", async function saveInput(testEmail){
+            var sms = () => {
+              switch(carrier.toLocaleLowerCase().trim()){
+                case "at&t": return phoneNum+"@txt.att.net"
+                case "verizon": return phoneNum+"@vtext.com"
+                case "sprint": return phoneNum+"@messaging.sprintpcs.com"
+                case "tmobile": return phoneNum+"@tmomail.net"
+                case "virgin mobile": return phoneNum+"@vmobl.com"
+                case "nextel": return phoneNum+"@messaging.nextel.com"
+                case "boost": return phoneNum+"@myboostmobile.com"
+                case "alltel": return phoneNum+"@message.alltel.com"
+                case "ee": return phoneNum+"@mms.ee.co.uk"
+                default: return "Invalid Carrier Input"
               }
-              fs.writeFileSync(path.resolve(__dirname, 'data.json'), JSON.stringify(data));
-              
-              if(stringToBoolean(testEmail)){
-                await sendMail(data)
-              }
+            } 
+            
+            let data = {
+              url : urls,
+              phoneNum : phoneNum,
+              carrier : carrier,
+              sms : sms(),
+              gmailUser: gmailUser,
+              gmailPass: gmailPass,
+              discordwebhook : discrodWebHook
+            }
+            fs.writeFileSync(path.resolve(__dirname, 'data.json'), JSON.stringify(data));
+            
+            if(stringToBoolean(testEmail)){
+              await sendMail(data)
+            }
 
-              rl.close();
-            })
+            rl.close();
           })
         })
       })
-    });
+    })
+  });
 
-    rl.on("close", function saveInput() {
-        console.log("\n[NVIDIA WATCHER] Saved to data.json");
-        process.exit(0);
-    });
+  rl.on("close", function saveInput() {
+      console.log("\n[NVIDIA WATCHER] Saved to data.json");
+      process.exit(0);
+  });
 }
 
 // Checks if data.json exists
--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const cheerio = require('cheerio') // Webscraping
 const nodemailer = require("nodemailer") // For sms
 let request = require("request"); // For url shortener
 const colors = require("colors")
-const { stringify } = require('querystring')
+const { Webhook, MessageBuilder } = require('discord-webhook-node');
+
+
 let consoleTitle = "[NVIDIA WATCHER] ".cyan
 
 function format(seconds) { //https://stackoverflow.com/questions/28705009/how-do-i-get-the-server-uptime-in-node-js
@@ -88,25 +90,58 @@ async function sendMail(gmailUser, gmailPass, recipiant, message) {
 }
 
 async function webScrape(url) {
+    //                            HTTPS:// || HTTP://       www.                        .com .net .org                  
+    let site = await url.replace(/(^\w+:|^)\/\//, "").replace("www.","").split(/(?:.com|.org|.net)[/?#]/)[0].toLowerCase() // to get domain host name
     try{
-    const html = await axios({
-        url: url,
-        method: 'get',
-        raxConfig: {
-            retry: 3,
-            retryDelay: 4000
-        }
-    })
-    //const html = await axios.get(url);
-    const $ = await cheerio.load(html.data);
+        const html = await axios({
+            url: url,
+            method: 'get',
+            raxConfig: {
+                retry: 3,
+                retryDelay: 4000
+            }
+        })
+       
+        //const html = await axios.get(url);
+        const $ = await cheerio.load(html.data);
 
-    var datahtml = {
-        url: url,
-        title: $('div.sku-title h1').text(),
-        button: $('div button.add-to-cart-button').text(),
+        if(site == "bestbuy"){
+            var datahtml = {
+                url: url,
+                site: site,
+                title: $('div.sku-title h1').text(),
+                shortTitle: $('div.sku-title h1').text().split(' ').slice(0,7).join(' '),
+                price: $('div.priceView-customer-price span').first().text(),
+                rating: $('span.ugc-c-review-average').text(),
+                button: $('div button.add-to-cart-button').text(),
+            }
+        }else{
+            var datahtml = {
+                url: url,
+                site: site,
+                title: "NOT SUPPORTED",
+                shortTitle: "NOT SUPPORTED",
+                price: "NOT SUPPORTED",
+                rating: "NOT SUPPORTED",
+                button: "NOT SUPPORTED",
+            }
+            return datahtml
+        }
+        /* Newegg has bot protection sadly :C https://i.imgur.com/eM7Wua7.png
+        if(site == "newegg"){
+            var datahtml = {
+                url: url,
+                site: site,
+                title: $('div.product-wrap h1.product-title').text(),
+                shortTitle: $('div.product-wrap h1.product-title').text().split(' ').slice(0,7).join(' '),
+                button: $('div.product-inventory strong').text().normalize("NFKC"),
+            } 
+        }
+        */
+        //console.log(datahtml) 
+        return datahtml
     }
-    return datahtml
-    }
+    
     catch (error){
         console.error(error.statusText)
     }
@@ -128,46 +163,97 @@ async function writeLog(text){
     }
 }
 
-var checkEvery = 1000 * 5 // seconds
+function checkStockTxt(txtFromSite){
+    switch(txtFromSite.trim()){
+        case "In stock.": case "Add to Cart": return true;
+        default: return false;
+    }
+}
+function sendWebHook(hookUrl, productSite, productURL, productShortTitle, productPrice, productRating){
+    
+    let date_ob =  new Date();
+    // YYYY-MM-DD HH:MM:SS format
+    let formattedTime = date_ob.getFullYear() + "-" + ('0'+date_ob.getMonth()).slice(-2) + "-" + ('0'+date_ob.getDate()).slice(-2) + " " + ('0' + date_ob.getHours()).slice(-2) + ":" + ('0' + date_ob.getMinutes()).slice(-2) + ":" +  ('0'+date_ob.getSeconds()).slice(-2)
+
+    // Converts number rating into stars
+    productRating = Math.fround(parseInt(productRating)) + 1
+    let stars = ""
+    for(let i = 0; i < productRating; i++){
+        stars += "⭐"
+    }
+    const hook = new Webhook(hookUrl);
+    const IMAGE_URL = 'https://viterbicareers.usc.edu/wp-content/uploads/2019/01/Nvidia-Logo.jpg';
+    hook.setUsername('NVIDIA WATCHER');
+    hook.setAvatar(IMAGE_URL);
+
+    const embed = new MessageBuilder()
+    .setTitle('📈 STONK ALERT ' + formattedTime)
+    .setDescription(` <@242889488785866752> \`${productSite.toUpperCase()} ${productShortTitle}\` **IS IN STOCK**`)
+    .setURL(`${productURL}`)
+    .setColor('#33FF00')
+    
+    .addField('Product', `${productShortTitle}`, true)
+    .addField('Price', `${productPrice}`, true)
+    .addField('Reviews', `${stars}`, true)
+    .setFooter('Made by smashguns#6175', 'https://cdn.discordapp.com/avatars/242889488785866752/40ee66d845e1a6341e03c450fcf6d221.png?size=256')
+    .setTimestamp();
+
+    hook.send(embed).catch(error =>{
+        if(error){
+            console.error(error)
+        }
+    })
+}
+
+// https://stackoverflow.com/questions/48432102/discord-js-cooldown-for-a-command-for-each-user-not-all-users  
+// Thanks for posting that ^ ive been using arrays instead of sets which breaks stuff
+function cooldown(url){
+    inCoolDownArr.add(url)
+    setTimeout(() => {
+        inCoolDownArr.delete(url)
+    },cooldownTime)
+}
+
+var checkEvery = 1000 * 4 // seconds
 var cooldownTime = 60000 * 5 // minutes
-var i = 0 // For counting how many times myloop() been called
-function myLoop() {
+var i = 0
+const inCoolDownArr = new Set();
+
+async function myLoop() {
     let rawdata = fs.readFileSync('data.json')
     let data = JSON.parse(rawdata)
     let urlArr = data["url"]
-    let sms = data["sms"]
+    let reciever = data["sms"]
     let gmailUser = data['gmailUser']
     let gmailPass = data['gmailPass']
-        urlArr.forEach( url => {
-            var timer = setInterval(async () => {
-                try{
-                    var webScrapeRes = await webScrape(url)  // Start the web scraping and return result || webScrapeRes = { url, title, button } 
-                    if(webScrapeRes == null && webScrapeRes == "") {return}
-                    let shortTitle = await webScrapeRes.title.split(' ').slice(0,7).join(' ') // Shortened title so its readable in SMS :D
-                    if(webScrapeRes.button == "Add to Cart"){ // If button from bestBuy = Add to Cart then
-                        
-                        console.log(`${consoleTitle}${shortTitle} :: ${colors.green("IN STOCK")} :: Run: ${i} :: Uptime: ${format(process.uptime())}`)
-                        console.log(colors.yellow(`${consoleTitle}${shortTitle} is now on  cooldown for ${cooldownTime / 60000 }m!`)) // CooldownTime is divided by 60000 because time math
-                        await sendMail(gmailUser, gmailPass, sms, `IN STOCK! ${shortTitle}`)
-                        await writeLog("[STOCK] " + shortTitle) // Writes to stock-logs.txt
-
-                        await setTimeout(() => {
-                            console.log(colors.magenta(`${consoleTitle}${shortTitle} is now out of cooldown` ))
-                            myLoop()
-                        }, cooldownTime);
-                        
-                        await clearInterval(timer)
-                    }else{
-                        console.log(`${consoleTitle}${shortTitle} :: ${colors.red("OUT OF STOCK")} :: Run: ${i} :: Uptime: ${format(process.uptime())}`)
-                    }
-                    i++
-                }catch(err){
-                    console.error(err)
-                }
-            }, checkEvery)
-        })
+    let webhook = data['discordwebhook']
     
+    
+
+    await urlArr.forEach( async function(url, index) {
+        setInterval(async function(){
+            await setTimeout( async function(){
+                if(inCoolDownArr.has(url)){return} // Put this above everything so it doesnt run a webScrape, takes up valuable time
+
+                var data = await webScrape(url)
+                if(data.title == "NOT SUPPORTED") {console.log(`${consoleTitle}${colors.red(`${data.site} NOT SUPPORTED!`)}`); return} // Check if website is supported
+
+                if(await checkStockTxt(data.button)){ 
+                    console.log(`${consoleTitle}[${colors.magenta(data.site.toUpperCase())}] ${data.shortTitle} :: ${colors.green(data.button)} :: Run: ${i} :: Uptime: ${format(process.uptime())}`)
+                    console.log(colors.yellow(`${consoleTitle}[${data.site.toUpperCase()}] ${data.shortTitle} is now on cooldown for ${cooldownTime / 60000 }m!`)) // CooldownTime is divided by 60000 because time math
+                    await sendMail(gmailUser, gmailPass, reciever, `IN STOCK! [${data.site.toUpperCase()}] ${data.shortTitle}`) // Sends mail to recipiant which is in data.json
+                    await writeLog(`[STOCK] [${data.site.toUpperCase()}] ${data.shortTitle}`) // Writes to stock-logs.txt
+                    if(webhook){ sendWebHook(webhook, data.site, data.url, data.shortTitle, data.price, data.rating)} // Sends webhook
+                    await cooldown(url) // Runs cooldown
+                    
+                }
+                else{ console.log(`${consoleTitle}[${colors.magenta(data.site.toUpperCase())}] ${data.shortTitle} :: ${colors.red(data.button)} :: Run: ${i} :: Uptime: ${format(process.uptime())}`) }
+                i++
+            }, index * checkEvery) // I used `index * checkEvery` so each site has atleast `checkEvery` seconds before running another site
+        },5000)
+    })
 }
+
 if (!fs.existsSync("data.json")) {
     runScript("./database.js", function(err) {
         if (err) throw err;
